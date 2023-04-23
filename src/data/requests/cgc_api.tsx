ea480c88ce788225cd5b0b534a2dfb9206854d45
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2023-04-23 21:05:34 +0800

    apis update

---
+++ src/data/requests/cgc_api.tsx
@@ -0,0 +1,24 @@
+export function znt() {
+    return (
+        fetch('https://api.geckoterminal.com/api/v2/networks/bsc/tokens/0xa3ec6031a97dc7b2fbce197cc84fbead9f9d4e8c/pools')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log('znt', data)
+                return data
+            })
+
+    );
+}
+
+
+export function dex_top_20() {
+    return (
+        fetch('https://api.geckoterminal.com/api/v2/networks/bsc/dexes/pancakeswap_v2/pools')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log('dex_top_20', data)
+                return data
+            })
+
+    );
+}
